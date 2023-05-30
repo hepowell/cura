f4ecab2a15128d47acef19f690f2477805cefe9b
Hannah Powell <hannah.powell.00@gmail.com>
2023-05-29 21:56:25 -0700

    added booking page

---
+++ app.js
@@ -0,0 +1,13 @@
+
+// Optional: Randomize animation delay for each letter
+const animatedLetters = document.querySelectorAll('.animated-letter');
+animatedLetters.forEach((letter) => {
+  letter.style.animationDelay = Math.random() + 's';
+});
+
+function playSong() {
+    setTimeout(function(){
+        const audio = document.getElementById("myAudio");
+        audio.play();
+    }, 4000);
+}
